1396c9915d6060937a86efd30ca915259e271766
Michael Raith <mraith@gmail.com>
2016-10-28 08:00:00 +0000

    add support for crypto.timingSafeEqual

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 
 'use strict';
 
+var crypto = require('crypto');
+
 /**
  * Do a constant time string comparison. Always compare the complete strings against each other to get a constant time.
  * This method does not short-cut if the two string's length differs.
@@ -12,7 +14,7 @@
  * @param {string} a
  * @param {string} b
  */
-module.exports = function safeCompare(a, b) {
+var safeCompare = function safeCompare(a, b) {
     var strA = String(a);
     var strB = String(b);
     var lenA = strA.length;
@@ -28,4 +30,21 @@ module.exports = function safeCompare(a, b) {
     }
 
     return result === 0;
+};
+
+// Node.JS v7 now implements a timing safe compare method. Return that if the user is running the latest Node.js version.
+module.exports = (crypto.timingSafeEqual !== 'undefined' ? crypto.timingSafeEqual : safeCompare);
+module.exports = function (a, b) {
+    if (crypto.timingSafeEqual !== 'undefined') {
+        var strA = String(a);
+        var strB = String(b);
+        
+        var key = crypto.pseudoRandomBytes(32);
+        var bufA = crypto.createHmac('sha256', key).update(strA).digest();
+        var bufB = crypto.createHmac('sha256', key).update(strB).digest();
+        
+        return crypto.timingSafeEqual.call(crypto.timingSafeEqual, bufA, bufB);
+    } else {
+        return safeCompare;
+    }
 };
